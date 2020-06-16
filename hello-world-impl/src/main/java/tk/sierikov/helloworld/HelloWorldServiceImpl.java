5d75c589c6715f865af0504fe438ec48bf60c4ff
sierikov <artem.sierikov@gmail.com>
2020-06-16 04:25:29 +0300

    Added Hello World Implementation via TDD 2

--- hello-world-impl/src/main/java/tk/sierikov/helloworld/HelloWorldServiceImpl.java
+++ hello-world-impl/src/main/java/tk/sierikov/helloworld/HelloWorldServiceImpl.java
@@ -1,1 +1,13 @@
-package tk.sierikov
+package tk.sierikov.helloworld;
+
+import akka.NotUsed;
+import com.lightbend.lagom.javadsl.api.ServiceCall;
+
+import static java.util.concurrent.CompletableFuture.completedFuture;
+
+public class HelloWorldServiceImpl implements HelloWorldService {
+    @Override
+    public ServiceCall<NotUsed, String> helloWorld(){
+        return notUsed -> completedFuture("Hello World");
+    }
+}
